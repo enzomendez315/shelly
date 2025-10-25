4281e16a3d67176eeec37f4471b3db47b395f1e3
enzomendez315 <enzo.mendez315@gmail.com>
2025-10-24 21:31:15 -0600

    Add echo command.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Testing the setup!");
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function run() {
+  rl.question("$ ", (command) => {
+    command = command.trim()
+
+    if (command === "exit 0") {
+      process.exit(0);
+    }
+    else if (command.startsWith("echo")) {
+      const message = command.slice(4).trim();
+      console.log(message);
+    }
+    else {
+      rl.write(`${command}: command not found\n`);
+    }
+
+    run();
+  });
+}
+
+run();
