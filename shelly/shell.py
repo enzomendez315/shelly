6efad4e54c34d78ba4cf902daac6463608548f5a
enzomendez315 <enzo.mendez315@gmail.com>
2025-11-16 20:06:28 -0700

    Add echo and exit.

--- shelly/shell.py
+++ shelly/shell.py
@@ -7,3 +7,28 @@ class Shell():
 
     def run(self):
         sys.stdout.write("$ ")
+        sys.stdout.flush()
+
+        while True:
+            command = sys.stdin.readline().strip()
+            line = command.split(" ")
+            
+            self.handle_command(line)
+            
+            sys.stdout.write("$ ")
+            sys.stdout.flush()
+
+    def handle_command(self, line: list[str]):
+        if not line:
+            return
+        
+        cmd = line[0]
+        args = " ".join(line[1:])
+
+        match cmd:
+            case "exit":
+                sys.exit()
+            case "echo":
+                sys.stdout.write(f"{args}\n")
+            case _:
+                sys.stdout.write(f"{cmd}: command not found\n")
